a10e5c527a5b4b5a470cc3ee901df3a1e4016c27
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2021-08-26 15:50:40 -0300

    Adiciona ecercicios do bloco-04.1 e exercicios de fixacao do bloco-04.2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/bloco-04.1/exercicio-1/javascript.js
@@ -0,0 +1,10 @@
+const myName = "Pedro Ivo";
+const birthCity = "Teófilo Otoni";
+let birthYear = 1999;
+birthYear = 2000;
+birthCity = "Governador Valadares";
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/exercicios-de-fixacao.js
@@ -0,0 +1,31 @@
+/* Obtenha o valor "Serviços" do array menu :
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[1];
+
+console.log(indexOfPortfolio); */
+
+/* Procure o índice do valor "Portfólio" do array menu
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio") ;
+
+console.log(indexOfPortfolio); */
+
+/* Adicione o valor "Contato" no final do array menu :
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("Contato")
+
+console.log(menu); */
+
+/* Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (var index= 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+} */
+
+/* Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nome of names) {
+    console.log(nome);
+} */ 
